36cdc6613ae3fc1d577e3160c4d4f342dd1488bd
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-17 18:26:40 +0700

    feat(web): add query state matching on tabs link component

--- apps/web/app/(columns)/(search)/search/page.tsx
+++ apps/web/app/(columns)/(search)/search/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { BackHeader } from "@/components/back-header";
-import { SearchBar } from "@/components/search-bar";
-import { SearchResults } from "@/components/search-results";
-import { Button } from "@semicolon/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@semicolon/ui/tabs";
-import { Ellipsis } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -34,39 +28,5 @@ export default function Page() {
     router.push(`/search?q=${newQuery}`);
   };
 
-  return (
-    <Tabs
-      className="flex h-full flex-col"
-      defaultValue={ActiveTab.Top}
-      onValueChange={(value) => setActiveTab(value as ActiveTab)}
-    >
-      <div className="bg-card sticky top-0 z-40 flex flex-col gap-1">
-        <BackHeader className="flex flex-row px-3">
-          <SearchBar onSearch={handleSearch} initialValue={query} />
-          <Button
-            variant="ghost"
-            size="icon"
-            className="aspect-square size-fit rounded-full p-2"
-          >
-            <Ellipsis className="flex-none" size={19} />
-          </Button>
-        </BackHeader>
-        <TabsList>
-          <TabsTrigger value={ActiveTab.Top}>Top</TabsTrigger>
-          <TabsTrigger value={ActiveTab.Latest}>Latest</TabsTrigger>
-          <TabsTrigger value={ActiveTab.People}>People</TabsTrigger>
-        </TabsList>
-      </div>
-      <TabsContent value={ActiveTab.Top}>
-        <div className="mb-4 flex flex-col">
-          <SearchResults query={query} activeTab={ActiveTab.Top} />
-        </div>
-      </TabsContent>
-      <TabsContent value={ActiveTab.Latest}>
-        <div className="mb-4 flex flex-col">
-          <SearchResults query={query} activeTab={ActiveTab.Latest} />
-        </div>
-      </TabsContent>
-    </Tabs>
-  );
+  return <div>hello world</div>;
 }
--- apps/web/components/search-bar.tsx
+++ apps/web/components/search-bar.tsx
@@ -2,39 +2,17 @@
 
 import { Input } from "@semicolon/ui/input";
 import { Search } from "lucide-react";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export function SearchBar() {
-  const [value, setValue] = useState(initialValue);
-
-  useEffect(() => {
-    setValue(initialValue);
-  }, [initialValue]);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (onSearch) {
-      onSearch(value);
-    }
-  };
-
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="relative flex flex-grow items-center justify-start"
-    >
+    <form className="relative flex flex-grow items-center justify-start">
       <Search className="absolute left-6 block items-center" />
       <Input
         type="text"
         placeholder="Search"
         id="search"
         className="h-12 flex-grow rounded-full bg-transparent pl-16 text-base text-white"
-        value={value}
-        onChange={handleChange}
       />
     </form>
   );
