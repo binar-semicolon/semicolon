bb75eb194f9d23a2328d049c7ce7a00e559c9094
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-07-17 14:52:41 +0700

    refactor: remove all build cache

--- apps/web/.next/server/interception-route-rewrite-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__INTERCEPTION_ROUTE_REWRITE_MANIFEST="[{\"source\":\"/:username/post/:id/photo/:num\",\"destination\":\"/(.):username/post/:id/photo/:num\",\"has\":[{\"type\":\"header\",\"key\":\"Next-Url\",\"value\":\"(?:\\\\/(.*))?[\\\\/#\\\\?]?\"}]}]"
